794d267c4e10bfcff2300f0f43b885783dbb5ff4
ishal24 <faishalrizqy24@gmail.com>
2024-05-16 20:07:46 +0700

    update net config

--- Network Configuration/1. Arakis.sh
+++ Network Configuration/1. Arakis.sh
@@ -5,23 +5,23 @@ iface eth0 inet dhcp
 # Harkonen
 auto eth1
 iface eth1 inet static
-    address 10.70.1.1
+    address 10.70.1.0
     netmask 255.255.255.0
 
 # Atreides
 auto eth2
 iface eth2 inet static
-    address 10.70.2.1
+    address 10.70.2.0
     netmask 255.255.255.0
 
 # Corrino
 auto eth3
 iface eth3 inet static
-    address 10.70.3.1
+    address 10.70.3.0
     netmask 255.255.255.0
 
 # Fremen
 auto eth4
 iface eth4 inet static
-    address 10.70.4.1
+    address 10.70.4.0
     netmask 255.255.255.0
--- Network Configuration/2. Server and Load Balancer.sh
+++ Network Configuration/2. Server and Load Balancer.sh
@@ -2,28 +2,28 @@
 # Stilgar - Load Balancer
 auto eth0
 iface eth0 inet static
-    address 10.70.4.2
+    address 10.70.4.1
     netmask 255.255.255.0
-    gateway 10.70.4.1
+    gateway 10.70.4.0
 
 # Server
 # Mohiam - DHCP Server
 auto eth0
 iface eth0 inet static
-    address 10.70.3.3
+    address 10.70.3.2
     netmask 255.255.255.0
-    gateway 10.70.3.1
+    gateway 10.70.3.0
 
 # Irulan - DNS Server
 auto eth0
 iface eth0 inet static
-    address 10.70.3.2
+    address 10.70.3.1
     netmask 255.255.255.0
-    gateway 10.70.3.1
+    gateway 10.70.3.0
 
 # Chani - Database Server
 auto eth0
 iface eth0 inet static
-    address 10.70.4.3
+    address 10.70.4.2
     netmask 255.255.255.0
-    gateway 10.70.4.1
+    gateway 10.70.4.0
